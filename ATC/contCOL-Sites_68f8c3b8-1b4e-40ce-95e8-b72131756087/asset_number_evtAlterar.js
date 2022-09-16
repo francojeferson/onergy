1149934925b8098a8b5ca104601f24ab56a2222c
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-16 14:28:54 -0300

    update asset_number_evtAlterar.js

--- ATC/contCOL-Sites_68f8c3b8-1b4e-40ce-95e8-b72131756087/asset_number_evtAlterar.js
+++ ATC/contCOL-Sites_68f8c3b8-1b4e-40ce-95e8-b72131756087/asset_number_evtAlterar.js
@@ -1,13 +1,17 @@
+let onergyCtx = mtdOnergy.JsEvtGetCurrentCtx();
+
 let mainMethod = async () => {
-    // debugger;
+    debugger;
     let cms = mtdOnergy.JsEvtGetItemValue('asset_number');
     let objCms = await mtdOnergy.JsEvtGetFeedData({
         fdtID: 'e43b9fe0-6752-446d-8495-0b4fdd7a70b4',
         filter: JSON.stringify([{ FielName: 'asset_number', Type: 'string', FixedType: 'string', Value1: cms }]),
     });
     if (objCms.length > 0 && cms == objCms[0].urlJsonContext.asset_number) {
-        mtdOnergy.JsEvtShowMessage('error', 'Asset Number ya informado');
-        mtdOnergy.JsEvtShowHideLoading(false);
+        if (onergyCtx.fedid != objCms[0].id) {
+            mtdOnergy.JsEvtShowMessage('error', 'Asset Number ya informado');
+            mtdOnergy.JsEvtShowHideLoading(false);
+        }
     }
 };
 
