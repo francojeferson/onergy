51ce4d7ddc1ae729da56790750cc19a73c1eff07
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-16 18:13:21 -0300

    update evtAlterar.js

--- ATC/contCOL-Sites_68f8c3b8-1b4e-40ce-95e8-b72131756087/evtAlterar.js
+++ ATC/contCOL-Sites_68f8c3b8-1b4e-40ce-95e8-b72131756087/evtAlterar.js
@@ -1,11 +1,11 @@
 let onergyCtx = mtdOnergy.JsEvtGetCurrentCtx();
 
 let mainMethod = async () => {
-    debugger;
+    let sitiosID = 'e43b9fe0-6752-446d-8495-0b4fdd7a70b4';
     let cms = mtdOnergy.JsEvtGetItemValue('asset_number');
     let objCms = await mtdOnergy.JsEvtGetFeedData({
-        fdtID: 'e43b9fe0-6752-446d-8495-0b4fdd7a70b4',
-        filter: JSON.stringify([{ FielName: 'asset_number', Type: 'string', FixedType: 'string', Value1: cms }]),
+        fdtID: sitiosID,
+        filter: gerarFiltro('asset_number', cms),
     });
     if (objCms.length > 0 && cms == objCms[0].urlJsonContext.asset_number) {
         if (onergyCtx.fedid != objCms[0].id) {
@@ -13,6 +13,17 @@ let mainMethod = async () => {
             mtdOnergy.JsEvtShowHideLoading(false);
         }
     }
+};
+
+const gerarFiltro = (fielNameP, valueP) => {
+    return JSON.stringify([
+        {
+            FielName: fielNameP,
+            Type: `${typeof valueP == 'number' ? 'Numeric' : 'string'}`,
+            FixedType: `${typeof valueP == 'number' ? 'Numeric' : 'string'}`,
+            Value1: valueP,
+        },
+    ]);
 };
 
 mainMethod();
