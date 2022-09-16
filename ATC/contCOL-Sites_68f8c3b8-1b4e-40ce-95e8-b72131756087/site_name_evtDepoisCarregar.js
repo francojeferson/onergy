a63daa6a6e79f712d42b06cbe17ba5eaaf67c359
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-16 11:27:21 -0300

    feat: create site_name_evtDepoisCarregar.js

---
+++ ATC/contCOL-Sites_68f8c3b8-1b4e-40ce-95e8-b72131756087/site_name_evtDepoisCarregar.js
@@ -0,0 +1,8 @@
+let mainMethod = () => {
+    /*
+    let contextData = mtdOnergy.JsEvtGetCurrentCtx();
+    mtdOnergy.JsEvtSetItemValue("onergyteam_equipe", contextData.userContext.perfil[0].perfilName);
+    mtdOnergy.JsEvtSetItemValue("onergyteam_id", contextData.userContext.perfil[0].grpId);
+*/
+};
+mainMethod();
