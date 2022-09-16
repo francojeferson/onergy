c86ec9c82c5614a7124e8b28bc358ad097f23ef6
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-16 14:39:28 -0300

    chore: delete site_name_evtDepoisCarregar.js

--- ATC/contCOL-Sites_68f8c3b8-1b4e-40ce-95e8-b72131756087/site_name_evtDepoisCarregar.js
+++
@@ -1,8 +0,0 @@
-let mainMethod = () => {
-    /*
-    let contextData = mtdOnergy.JsEvtGetCurrentCtx();
-    mtdOnergy.JsEvtSetItemValue("onergyteam_equipe", contextData.userContext.perfil[0].perfilName);
-    mtdOnergy.JsEvtSetItemValue("onergyteam_id", contextData.userContext.perfil[0].grpId);
-*/
-};
-mainMethod();
