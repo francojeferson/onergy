bcacd86623d7135841b4e7cf1ea1383d45274e3f
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-17 00:29:04 -0300

    update evtAntesSalvar.js

--- ATC/contCOL-InformacionesdelaCuenta(new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAntesSalvar.js
+++ ATC/contCOL-InformacionesdelaCuenta(new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAntesSalvar.js
@@ -1,8 +1,10 @@
 let mainMethod = () => {
+    //! Campo usado para bloquear edição de outros campos
     let registroSalvo = mtdOnergy.JsEvtGetItemValue('registro_salvo');
     if (!registroSalvo || registroSalvo == 'nao') {
         mtdOnergy.JsEvtSetItemValue('registro_salvo', 'sim');
     }
+    //!
 
     mtdOnergy.JsEvtSubmitForm();
 };
