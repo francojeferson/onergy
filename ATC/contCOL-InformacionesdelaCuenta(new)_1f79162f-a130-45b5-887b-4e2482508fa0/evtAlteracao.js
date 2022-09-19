3c948ec8971b22af65f6ef576adb76a5e548266b
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-19 17:28:34 -0300

    update evtAlteracao.js

--- ATC/contCOL-InformacionesdelaCuenta(new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAlteracao.js
+++ ATC/contCOL-InformacionesdelaCuenta(new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAlteracao.js
@@ -1,15 +1,22 @@
 let onergyCtx = mtdOnergy.JsEvtGetCurrentCtx();
 
 let mainMethod = async () => {
-    debugger;
-    let cms = mtdOnergy.JsEvtGetItemValue('asset_number');
-    mtdOnergy.JsEvtSetItemValue('asset_number_cache', cms);
-    let tipoConta = mtdOnergy.JsEvtGetItemValue('TCTC_tipo_de_conta__TC_tipo_de_conta_valor');
-    if (tipoConta == 'P') {
-        mtdOnergy.JsEvtSetItemValue('asset_number', '');
-    } else {
-        let cms_cache = mtdOnergy.JsEvtGetItemValue('asset_number_cache');
-        mtdOnergy.JsEvtSetItemValue('asset_number', cms_cache);
+    let tipoConta = mtdOnergy.JsEvtGetItemValue('TCprcs__tipo_de_conta_id');
+    let tipoContaCache = mtdOnergy.JsEvtGetItemValue('prcs__tipo_de_conta_cache');
+    if (tipoConta != tipoContaCache) {
+        let tipoContaValue = mtdOnergy.JsEvtGetItemValue('TCTC_tipo_de_conta__TC_tipo_de_conta_valor');
+        mtdOnergy.JsEvtSetItemValue('prcs__tipo_de_conta_cache', tipoConta);
+
+        if (tipoContaValue == 'P') {
+            let assetNumber = mtdOnergy.JsEvtGetItemValue('asset_number');
+            mtdOnergy.JsEvtSetItemValue('asset_number_cache', assetNumber);
+            mtdOnergy.JsEvtSetItemValue('asset_number', '');
+        } else {
+            let assetNumberCache = mtdOnergy.JsEvtGetItemValue('asset_number_cache');
+            if (assetNumberCache) {
+                mtdOnergy.JsEvtSetItemValue('asset_number', assetNumberCache);
+            }
+        }
     }
 };
 
