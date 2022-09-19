b864b1725b0aa67de92480cfad05fba0aa57db83
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-19 20:21:01 -0300

    update evtAlteracao.js

--- ATC/contCOL-InformacionesdelaCuenta(new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAlteracao.js
+++ ATC/contCOL-InformacionesdelaCuenta(new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAlteracao.js
@@ -1,6 +1,22 @@
 let onergyCtx = mtdOnergy.JsEvtGetCurrentCtx();
 
 let mainMethod = async () => {
+    await validarTipoConta();
+    await validarDiaPago();
+};
+
+let validarDiaPago = async () => {
+    let diaPago = mtdOnergy.JsEvtGetItemValue('prcs__dia_de_pagamento');
+    // se diaPago for diferente de entre 1 e 31, mostrar mensagem de erro
+    if (diaPago < 1 || diaPago > 31) {
+        mtdOnergy.JsEvtShowMessage('error', 'Día de Pago inválido');
+        mtdOnergy.JsEvtShowHideLoading(false);
+        return false;
+    }
+    return true;
+};
+
+let validarTipoConta = async () => {
     let tipoConta = mtdOnergy.JsEvtGetItemValue('TCprcs__tipo_de_conta_id');
     let tipoContaCache = mtdOnergy.JsEvtGetItemValue('prcs__tipo_de_conta_cache');
     if (tipoConta != tipoContaCache) {
