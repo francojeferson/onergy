581cde291a67c9762b891b18d11ac9110ccb5131
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-20 10:41:52 -0300

    update evtAlteracao.js

--- ATC/contCOL-InformacionesdelaCuenta(new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAlteracao.js
+++ ATC/contCOL-InformacionesdelaCuenta(new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAlteracao.js
@@ -1,13 +1,31 @@
 let onergyCtx = mtdOnergy.JsEvtGetCurrentCtx();
 
 let mainMethod = async () => {
-    await validarTipoConta();
     await validarDiaPago();
+    await limiteDiaPago();
+    await validarTipoConta();
 };
 
 let validarDiaPago = async () => {
+    let provedorID = '4783ca0b-357d-42ab-a5c8-3328ee315f86';
+    let nomeProvedor = mtdOnergy.JsEvtGetItemValue('prvd_id');
+    let nomeProvedorCache = mtdOnergy.JsEvtGetItemValue('nome_provedor_id_cache');
+    if (nomeProvedor != nomeProvedorCache) {
+        mtdOnergy.JsEvtSetItemValue('nome_provedor_id_cache', nomeProvedor);
+        if (nomeProvedor) {
+            let objProvedor = await mtdOnergy.JsEvtGetFeedData({
+                fdtID: provedorID,
+                filter: gerarFiltro('_id', nomeProvedor),
+            });
+            if (objProvedor && objProvedor.length > 0) {
+                mtdOnergy.JsEvtSetItemValue('prcs__dia_de_pagamento', objProvedor[0].urlJsonContext.dia_de_vencimento);
+            }
+        }
+    }
+};
+
+let limiteDiaPago = async () => {
     let diaPago = mtdOnergy.JsEvtGetItemValue('prcs__dia_de_pagamento');
-    // se diaPago for diferente de entre 1 e 31, mostrar mensagem de erro
     if (diaPago < 1 || diaPago > 31) {
         mtdOnergy.JsEvtShowMessage('error', 'Día de Pago inválido');
         mtdOnergy.JsEvtShowHideLoading(false);
