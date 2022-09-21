90148e33d786a148d7a1c1b277ea8a36dbaece79
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-21 18:55:32 -0300

    update evtAlteracao_prcs__tipo_de_conta.js

--- ATC/form_COL-Informaciones de la Cuenta (new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAlteracao_prcs__tipo_de_conta.js
+++ ATC/form_COL-Informaciones de la Cuenta (new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAlteracao_prcs__tipo_de_conta.js
@@ -1,4 +1,5 @@
 let mainMethod = async () => {
+    await validarTipoConta();
     await validarContaPai();
 };
 
@@ -19,6 +20,38 @@ let validarTipoConta = async () => {
             let assetNumber = mtdOnergy.JsEvtGetItemValue('asset_number');
             mtdOnergy.JsEvtSetItemValue('asset_number_IDC', assetNumber);
         }
+    }
+};
+
+// Valida o tipo de conta e seta o valor do campo prcs__conta_pai
+let validarContaPai = async () => {
+    let tipoContaValue = mtdOnergy.JsEvtGetItemValue('TCTC_tipo_de_conta__TC_tipo_de_conta_valor');
+    let contaInternaNIC = mtdOnergy.JsEvtGetItemValue('conta_interna_nic');
+
+    if (tipoContaValue) {
+        // Verifica se Tipo de Cuenta é I (Individual) e se for, prcs__conta_pai deve receber NO
+        if (tipoContaValue == 'I') {
+            mtdOnergy.JsEvtSetItemValue('prcs__conta_pai', 'NO');
+        }
+
+        // Verifica se Tipo de Cuenta é P (Padre) ou HH (Hija Hibrida) e se for, seta Cuenta Interna NIC em campo prcs__conta_pai
+        else if (tipoContaValue == 'P' || tipoContaValue == 'HH') {
+            mtdOnergy.JsEvtSetItemValue('prcs__conta_pai', contaInternaNIC);
+        }
+
+        // Verifica se Tipo de Cuenta é H (Hija) e se for, Cuenta Interna NIC deve ser diferente de prcs__conta_pai
+        else if (tipoContaValue == 'H') {
+            let contaPai = mtdOnergy.JsEvtGetItemValue('prcs__conta_pai');
+
+            // Verifica se Cuenta Interna NIC é diferente de prcs__conta_pai, se for, exibe mensagem de erro e retorna false
+            if (contaPai == contaInternaNIC) {
+                mtdOnergy.JsEvtShowMessage('error', 'Cuenta Padre no puede ser igual a Cuenta Interna NIC para Tipo de Cuenta Hija');
+                mtdOnergy.JsEvtSetItemValue('prcs__conta_pai', '');
+                mtdOnergy.JsEvtShowHideLoading(false);
+                return false;
+            }
+        }
+        return true;
     }
 };
 
