4f71eb90c8836695e36877b1c06968efb6c737ac
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-21 15:18:22 -0300

    update evtAlteracao_prcs__dia_de_pagamento.js

--- ATC/form_COL-Informaciones de la Cuenta (new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAlteracao_prcs__dia_de_pagamento.js
+++ ATC/form_COL-Informaciones de la Cuenta (new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAlteracao_prcs__dia_de_pagamento.js
@@ -8,8 +8,7 @@ let mainMethod = async () => {
 let limiteDiaPago = async () => {
     let diaPago = mtdOnergy.JsEvtGetItemValue('prcs__dia_de_pagamento');
 
-    // Verifica se o dia de pagamento é maior que 31 e menor que 1 e se for, 
-    // envia uma mensagem de erro durante alteração
+    // Verifica se o dia de pagamento é maior que 31 e menor que 1 e se for, envia uma mensagem de erro durante alteração
     if (diaPago < 1 || diaPago > 31) {
         mtdOnergy.JsEvtShowMessage('error', 'Día de Pago inválido');
         mtdOnergy.JsEvtShowHideLoading(false);
