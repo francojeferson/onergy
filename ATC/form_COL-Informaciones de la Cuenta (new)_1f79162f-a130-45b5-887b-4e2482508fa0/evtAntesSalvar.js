fed0e6b23808331efd3bc0dac723a0e6274f4862
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-21 15:18:00 -0300

    update evtAntesSalvar.js

--- ATC/form_COL-Informaciones de la Cuenta (new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAntesSalvar.js
+++ ATC/form_COL-Informaciones de la Cuenta (new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAntesSalvar.js
@@ -12,8 +12,7 @@ let mainMethod = async () => {
 let blockFieldEdition = async () => {
     let registroSalvo = mtdOnergy.JsEvtGetItemValue('registro_salvo_');
 
-    // Verifica se o registro não foi salvo e se não foi, bloqueia a edição 
-    // dos campos através do registro_salvo_
+    // Se registro_salvo_ for igual a nao ou null, seta registro_salvo_ para sim e cfgCondicionais altera para somente leitura os campos que não podem ser editados pelo usuário
     if (!registroSalvo || registroSalvo == 'nao') {
         mtdOnergy.JsEvtSetItemValue('registro_salvo_', 'sim');
     }
@@ -23,8 +22,7 @@ let blockFieldEdition = async () => {
 let limiteDiaPago = async () => {
     let diaPago = mtdOnergy.JsEvtGetItemValue('prcs__dia_de_pagamento');
 
-    // Verifica se o dia de pagamento é maior que 31 e menor que 1 e se for, 
-    // envia uma mensagem de erro e cancela o submit
+    // Verifica se o dia de pagamento é maior que 31 e menor que 1 e se for, envia uma mensagem de erro e cancela o submit
     if (diaPago < 1 || diaPago > 31) {
         mtdOnergy.JsEvtShowMessage('error', 'Día de Pago inválido');
         mtdOnergy.JsEvtShowHideLoading(false);
