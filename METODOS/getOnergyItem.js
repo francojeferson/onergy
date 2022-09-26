6437b9977f4d20b6d789d434f4f7d846586df1dd
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-26 09:43:47 -0300

    feat: create getOnergyItem.js

---
+++ METODOS/getOnergyItem.js
@@ -0,0 +1,31 @@
+let fdtidDoCardDeBusca = '';
+
+let strFiltro = JSON.stringify([{ FielName: 'campo_de_busca', Type: 'string', FixedType: 'string', Value1: valor_do_filtro }]);
+
+let cadastroLOG = await getOnergyItem(fdtidDoCardDeBusca, data.onergy_js_ctx.assid, data.onergy_js_ctx.usrid, strFiltro);
+
+async function getOnergyItem(fdtid, assid, usrid, filtro) {
+    let keepSearching = true;
+    let skip = 0;
+    take = 500;
+    let result = [];
+    while (keepSearching) {
+        let strPageResp = await onergy_get({
+            fdtid: fdtid,
+            assid: assid,
+            usrid: usrid,
+            filter: filtro,
+            skip: skip,
+            take: take,
+        });
+        skip += take;
+        let pageResp = JSON.parse(strPageResp);
+        if (pageResp != null && pageResp.length > 0) {
+            keepSearching = pageResp.length == take;
+            result = result.concat(pageResp);
+        } else {
+            keepSearching = false;
+        }
+    }
+    return await result;
+}
