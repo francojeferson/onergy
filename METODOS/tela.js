3cb1f1ddf41bf46b4bde1b52cdfe17161a257ed6
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-26 09:39:48 -0300

    feat: create tela.js

---
+++ METODOS/tela.js
@@ -0,0 +1,4 @@
+mtdOnergy.JsEvtGetItemValue('FLM_codigo_filial');
+mtdOnergy.JsEvtSetItemValue('FLM_codigo_filial', 'valor para atribuir');
+this.JsEvtShowHideLoading(false);
+this.JsEvtSubmitForm();
