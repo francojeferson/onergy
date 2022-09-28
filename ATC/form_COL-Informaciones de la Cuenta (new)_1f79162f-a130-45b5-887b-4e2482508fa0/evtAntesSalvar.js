740a8229c171ca3eeb5761c25dcb25d5debd01ac
jeferson-franco <jefersonfranco@pm.me>
2022-09-28 15:55:57 -0300

    update evtAntesSalvar.js

--- ATC/form_COL-Informaciones de la Cuenta (new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAntesSalvar.js
+++ ATC/form_COL-Informaciones de la Cuenta (new)_1f79162f-a130-45b5-887b-4e2482508fa0/evtAntesSalvar.js
@@ -15,21 +15,21 @@ let mainMethod = async () => {
 
 // Bloqueia a edição de campos que não podem ser editados pelo usuário
 let condSomenteLeitura = async () => {
-    let registroSalvo = mtdOnergy.JsEvtGetItemValue('registro_salvo_');
+    let registroSalvo = mtdOnergy.JsEvtGetItemValue("registro_salvo_");
 
     // Se registro_salvo_ for igual a nao ou null, seta registro_salvo_ para sim e cfgCondicionais altera para somente leitura os campos que não podem ser editados pelo usuário
-    if (!registroSalvo || registroSalvo == 'nao') {
-        mtdOnergy.JsEvtSetItemValue('registro_salvo_', 'sim');
+    if (!registroSalvo || registroSalvo == "nao") {
+        mtdOnergy.JsEvtSetItemValue("registro_salvo_", "sim");
     }
 };
 
 // Valida o limite de dia de pagamento
 let limiteDiaPago = async () => {
-    let diaPago = mtdOnergy.JsEvtGetItemValue('prcs__dia_de_pagamento');
+    let diaPago = mtdOnergy.JsEvtGetItemValue("prcs__dia_de_pagamento");
 
     // Verifica se o dia de pagamento é maior que 31 e menor que 1 e se for, envia uma mensagem de erro e cancela o submit
     if (diaPago < 1 || diaPago > 31) {
-        mtdOnergy.JsEvtShowMessage('error', 'Día de Pago inválido');
+        mtdOnergy.JsEvtShowMessage("error", "Día de Pago inválido");
         mtdOnergy.JsEvtShowHideLoading(false);
         return false;
     }
@@ -38,17 +38,17 @@ let limiteDiaPago = async () => {
 
 // Valida se Cuenta Interna (NIC) já foi informada
 let validarNic = async () => {
-    let informacoesContaID = '1e6d6595-083f-4bb8-b82c-e9054e9dc8f3';
-    let nic = mtdOnergy.JsEvtGetItemValue('conta_interna_nic');
+    let informacoesContaID = "1e6d6595-083f-4bb8-b82c-e9054e9dc8f3";
+    let nic = mtdOnergy.JsEvtGetItemValue("conta_interna_nic");
     let objNic = await mtdOnergy.JsEvtGetFeedData({
         fdtID: informacoesContaID,
-        filter: gerarFiltro('conta_interna_nic', nic),
+        filter: gerarFiltro("conta_interna_nic", nic),
     });
 
     // Se o conta_interna_nic já foi informado, exibe uma mensagem de erro
     if (objNic.length > 0 && nic == objNic[0].urlJsonContext.conta_interna_nic) {
         if (onergyCtx.fedid != objNic[0].id) {
-            mtdOnergy.JsEvtShowMessage('error', 'Cuenta Interna (NIC) ya informada');
+            mtdOnergy.JsEvtShowMessage("error", "Cuenta Interna (NIC) ya informada");
             mtdOnergy.JsEvtShowHideLoading(false);
             return false;
         }
@@ -61,8 +61,8 @@ const gerarFiltro = (fielNameP, valueP) => {
     return JSON.stringify([
         {
             FielName: fielNameP,
-            Type: `${typeof valueP == 'number' ? 'Numeric' : 'string'}`,
-            FixedType: `${typeof valueP == 'number' ? 'Numeric' : 'string'}`,
+            Type: `${typeof valueP == "number" ? "Numeric" : "string"}`,
+            FixedType: `${typeof valueP == "number" ? "Numeric" : "string"}`,
             Value1: valueP,
         },
     ]);
