bb3480c9501b82a9cd200c3a277ea391527bf378
jefersonfranco <jefersonfranco@protonmail.com>
2022-10-11 10:15:24 -0300

    feat: create 83-pagamento_aprovados_07e68c08-564a-439b-8d88-0ddc1c775b0d.js

---
+++ MITRE/proc_processo_c68cf814-cbfc-d721-fe10-a95f37c1c06b/83-pagamento_aprovados_07e68c08-564a-439b-8d88-0ddc1c775b0d.js
@@ -0,0 +1,22 @@
+function init(json) {
+    var data = JSON.parse(json);
+
+    //return true;
+    //return SetObjectResponse(true, data, true);
+}
+function initBefore(json) {
+    //return true;
+}
+function initDelete(json) {
+    //return true;
+}
+function SetObjectResponse(cond, json, WaitingWebHook) {
+    if (WaitingWebHook === undefined) WaitingWebHook = false;
+
+    var obj = {
+        cond: cond,
+        json: JSON.stringify(json),
+        WaitingWebHook: WaitingWebHook,
+    };
+    return obj;
+}
