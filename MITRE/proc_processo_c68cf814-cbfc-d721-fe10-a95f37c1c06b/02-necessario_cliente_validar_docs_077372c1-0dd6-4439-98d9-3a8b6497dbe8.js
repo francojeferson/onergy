1d7335fe064a96e141b4e2a7e3a2f50de8cb3b47
jefersonfranco <jefersonfranco@protonmail.com>
2022-10-11 10:14:18 -0300

    feat: create 02-necessario_cliente_validar_docs_077372c1-0dd6-4439-98d9-3a8b6497dbe8.js

---
+++ MITRE/proc_processo_c68cf814-cbfc-d721-fe10-a95f37c1c06b/02-necessario_cliente_validar_docs_077372c1-0dd6-4439-98d9-3a8b6497dbe8.js
@@ -0,0 +1,18 @@
+function init(json) {
+    var data = JSON.parse(json);
+
+    return data.necessario_validar_documentos;
+}
+function initBefore(json) {
+    //return true;
+}
+function SetObjectResponse(cond, json, WaitingWebHook) {
+    if (WaitingWebHook === undefined) WaitingWebHook = false;
+
+    var obj = {
+        cond: cond,
+        json: JSON.stringify(json),
+        WaitingWebHook: WaitingWebHook,
+    };
+    return obj;
+}
