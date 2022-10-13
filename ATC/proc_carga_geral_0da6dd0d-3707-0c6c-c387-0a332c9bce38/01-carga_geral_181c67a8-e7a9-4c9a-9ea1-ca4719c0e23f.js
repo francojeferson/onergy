cc4a7c7ea054745d98fdb4d2c09a6716a16d6ab4
jefersonfranco <jefersonfranco@protonmail.com>
2022-10-13 06:08:47 -0300

    update 01-carga_geral_181c67a8-e7a9-4c9a-9ea1-ca4719c0e23f.js

--- ATC/proc_carga_geral_0da6dd0d-3707-0c6c-c387-0a332c9bce38/01-carga_geral_181c67a8-e7a9-4c9a-9ea1-ca4719c0e23f.js
+++ ATC/proc_carga_geral_0da6dd0d-3707-0c6c-c387-0a332c9bce38/01-carga_geral_181c67a8-e7a9-4c9a-9ea1-ca4719c0e23f.js
@@ -1,15 +1,16 @@
-//!NODE_ENV ===
-const { date } = require('assert-plus');
-const { formatDate } = require('tough-cookie');
-const { log } = require('console');
-const { memory } = require('console');
-const { resolve } = require('path');
-const { type } = require('os');
-const axios = require('axios');
-const fs = require('fs');
-const jsuser = require('../../onergy/onergy-utils');
-const onergy = require('../../onergy/onergy-client');
-const utils = require('../../onergy/onergy-utils');
+/**NODE_ENV ===
+ */
+let { date } = require('assert-plus');
+let { formatDate } = require('tough-cookie');
+let { log } = require('console');
+let { memory } = require('console');
+let { resolve } = require('path');
+let { type } = require('os');
+let axios = require('axios');
+let fs = require('fs');
+let jsuser = require('../../onergy/onergy-utils');
+let onergy = require('../../onergy/onergy-client');
+let utils = require('../../onergy/onergy-utils');
 replaceAll = function (content, needle, replacement) {
     return content.split(needle).join(replacement);
 };
@@ -35,7 +36,7 @@ async function onergy_countdocs(args) {
     return await onergy.onergy_countdocs(args);
 }
 async function onergy_get(args) {
-    const r = await onergy.onergy_get(args);
+    let r = await onergy.onergy_get(args);
     return JSON.stringify(r);
 }
 async function onergy_save(args) {
@@ -61,11 +62,17 @@ function successCallback(result) {
     console.log('It succeeded with ' + result);
 }
 
-//!SCRIPT ===
-function init(json) {
-    var data = JSON.parse(json);
+/**SCRIPT ===
+ * Executar automático quando em processo: Não
+ * Atividade de longa duração: Não
+ * Esconder Menu: Sim
+ * Condicional: nenhum
+ * Aprovação: nenhum
+ */
+async function init(json) {
+    let data = JSON.parse(json);
     let execCargExcelFdtId = '0e8dc4f0-4a4f-4fb1-8268-423b45128203';
-    data.processamento = 'Processando';
+    data.processamento = 'Procesando'`${data.load_index_tab_excel}`;
 
     data.id_upload_planilha = data.onergy_js_ctx.fedid;
 
@@ -74,17 +81,16 @@ function init(json) {
     itemPost.assid = data.onergy_js_ctx.assid;
     itemPost.fdtid = execCargExcelFdtId;
     itemPost.usrid = data.onergy_js_ctx.usrid;
-    onergy_save(itemPost);
+    await onergy_save(itemPost);
     //return true;
     return SetObjectResponse(true, data, true);
 }
 function initBefore(json) {
     //return true;
 }
-
-function initDelete(json) {
+async function initDelete(json) {
     let data = JSON.parse(json);
-    var idSalvo = data.load_index_id_do_card;
+    let idSalvo = data.load_index_id_do_card;
     let postInfoDelet = {
         UrlJsonContext: {
             id_user_resp_delet: data.usrid,
@@ -92,19 +98,19 @@ function initDelete(json) {
         BlockCount: 1,
     };
     let excluirFilter = JSON.stringify([{ FielName: 'id_upload_planilha', Type: 'string', FixedType: 'string', Value1: data.onergy_js_ctx.fedid }]);
-    /*onergy_updatemany({
+    onergy_updatemany({
         fdtid: idSalvo,
         assid: data.onergy_js_ctx.assid,
         usrid: data.onergy_js_ctx.usrid,
         data: JSON.stringify(postInfoDelet),
         filter: excluirFilter,
-        isMultiUpdate: true
-    });*/
+        isMultiUpdate: true,
+    });
     return true;
 }
 function SetObjectResponse(cond, json, WaitingWebHook) {
     if (WaitingWebHook === undefined) WaitingWebHook = false;
-    var obj = {
+    let obj = {
         cond: cond,
         json: JSON.stringify(json),
         WaitingWebHook: WaitingWebHook,
@@ -112,6 +118,31 @@ function SetObjectResponse(cond, json, WaitingWebHook) {
     return obj;
 }
 
-//!METODOS PADRAO ===
-const json = {};
+/**METODOS PADRAO ===
+ */
+let json = {
+    load_index_equipe: 'COL',
+    load_index_id_do_card: '55ec978d-7dbe-4a6f-8cb4-536b53361d54',
+    planilha: [
+        {
+            Url: 'https://onebackupservices.blob.core.windows.net/67c0b77d-abae-4c48-ba4b-6c8faf27e14a/tablas_maestras.xlsxa347cd75-cda2-4054-b50e-cd2920750eb9.xlsx?sv=2018-03-28&sr=b&sig=MUgbTYDYvWmpzrxHesdEiPTz%2FjfThsbiMWrbLoJ8aAw%3D&se=2023-04-29T15%3A30%3A06Z&sp=r',
+            UrlAzure:
+                'https://onebackupservices.blob.core.windows.net/67c0b77d-abae-4c48-ba4b-6c8faf27e14a/tablas_maestras.xlsxa347cd75-cda2-4054-b50e-cd2920750eb9.xlsx?sv=2018-03-28&sr=b&sig=MUgbTYDYvWmpzrxHesdEiPTz%2FjfThsbiMWrbLoJ8aAw%3D&se=2023-04-29T15%3A30%3A06Z&sp=r',
+            Name: 'tablas_maestras.xlsx',
+        },
+    ],
+    load_index_tab_excel: 'categorias',
+    load_index_id: '72adb5cf-cbdc-4887-bedf-0ba2239e36dc',
+    em_caso_de_duplicidade: '1',
+    em_caso_de_duplicidade_desc: 'Sobrescribir',
+    ass_id: '67c0b77d-abae-4c48-ba4b-6c8faf27e14a',
+    assid: '67c0b77d-abae-4c48-ba4b-6c8faf27e14a',
+    email: 'admin-colombia@atc.com.co',
+    fdtid: '181c67a8-e7a9-4c9a-9ea1-ca4719c0e23f',
+    fedid: 'be530235-fdad-2222-14b8-94adf6be7b8b',
+    onergy_rolid: 'e4d0298c-245e-454a-89d4-8f27aef8645b',
+    timezone: null,
+    usrid: '0c44d4fc-d654-405b-9b8f-7fea162948b5',
+};
+
 init(JSON.stringify(json));
