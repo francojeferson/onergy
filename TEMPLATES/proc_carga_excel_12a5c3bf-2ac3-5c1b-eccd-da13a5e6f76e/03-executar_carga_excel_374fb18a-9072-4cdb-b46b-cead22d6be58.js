f19f115336248324ac0208025b7f1acf2fd0dacb
jefersonfranco <jefersonfranco@protonmail.com>
2022-10-13 06:09:43 -0300

    update 03-executar_carga_excel_374fb18a-9072-4cdb-b46b-cead22d6be58.js

--- TEMPLATES/proc_carga_excel_12a5c3bf-2ac3-5c1b-eccd-da13a5e6f76e/03-executar_carga_excel_374fb18a-9072-4cdb-b46b-cead22d6be58.js
+++ TEMPLATES/proc_carga_excel_12a5c3bf-2ac3-5c1b-eccd-da13a5e6f76e/03-executar_carga_excel_374fb18a-9072-4cdb-b46b-cead22d6be58.js
@@ -1,14 +1,106 @@
-function init(json) {
-    let data = JSON.parse(json);
+//!NODE_ENV ===
+const { date } = require('assert-plus');
+const { formatDate } = require('tough-cookie');
+const { log } = require('console');
+const { memory } = require('console');
+const { resolve } = require('path');
+const { type } = require('os');
+const axios = require('axios');
+const fs = require('fs');
+const jsuser = require('../../onergy/onergy-utils');
+const onergy = require('../../onergy/onergy-client');
+const utils = require('../../onergy/onergy-utils');
+replaceAll = function (content, needle, replacement) {
+    return content.split(needle).join(replacement);
+};
+async function ajax(args) {
+    return await onergy.ajax(args);
+}
+async function ajaxPost(args) {
+    return await onergy.ajaxPost(args);
+}
+function failureCallback(error) {
+    console.log('It failed with ' + error);
+}
+function get_usr_tmz_dt_now(data) {
+    return data;
+}
+async function hashMd5(args) {
+    return await onergy.hashMd5(args);
+}
+async function increment(args) {
+    return await onergy.increment(args);
+}
+async function onergy_countdocs(args) {
+    return await onergy.onergy_countdocs(args);
+}
+async function onergy_get(args) {
+    const r = await onergy.onergy_get(args);
+    return JSON.stringify(r);
+}
+async function onergy_save(args) {
+    return await onergy.onergy_save(args);
+}
+async function ReadExcelToJson(args) {
+    return await onergy.ReadExcelToJson(args);
+}
+async function ReadTextPdf(args) {
+    return await onergy.ReadTextPdf(args);
+}
+async function sendmail(args) {
+    return await onergy.sendmail(args);
+}
+async function onergy_sendto(args) {
+    let r = await onergy.onergy_sendto(args);
+    return JSON.stringify(r);
+}
+function onergy_updatemany(data) {
+    return data;
+}
+function successCallback(result) {
+    console.log('It succeeded with ' + result);
+}
+
+//!SCRIPT ===
+async function getOnergyItem(fdtid, assid, usrid, filtro) {
+    let keepSearching = true;
+    let skip = 0;
+    take = 500;
+    let result = [];
+    while (keepSearching) {
+        let strPageResp = await onergy_get({
+            fdtid: fdtid,
+            assid: assid,
+            usrid: usrid,
+            filter: filtro,
+            skip: skip,
+            take: take,
+        });
+        skip += take;
+        let pageResp = JSON.parse(strPageResp);
+        if (pageResp != null && pageResp.length > 0) {
+            keepSearching = pageResp.length == take;
+            result = result.concat(pageResp);
+        } else {
+            keepSearching = false;
+        }
+    }
+    return JSON.stringify(result);
+}
+
+async function init(json) {
+    const data = JSON.parse(json);
 
     let fdtdIdPost = data.CDPE_id_card;
 
     let arrayPost = [];
 
-    let strArrExcel = ReadExcelToJson({
+    let strArrExcel = await ReadExcelToJson({
         url: data.upload_planilha[0].UrlAzure,
     });
+
     let dataExcel = JSON.parse(strArrExcel);
+    let tabExcel = data.load_index_tab_excel;
 
     if (dataExcel != null) {
         let nomePlanilha = data.upload_planilha[0].Name;
@@ -43,7 +135,7 @@ function init(json) {
                     mensagem_erro: 'Não foi encontrado nenhuma aba com o nome "DADOS_CARGA_EXCEL".',
                 },
             };
-            onergy_updatemany({
+            await onergy_updatemany({
                 fdtid: '8b7dc946-2993-44e9-932a-947b77eb44cf',
                 assid: data.onergy_js_ctx.assid,
                 usrid: data.onergy_js_ctx.usrid,
@@ -76,7 +168,7 @@ function init(json) {
                 let y = 0;
             }
             */
-            onergy.InsertManyOnergy(arrayPost, fdtdIdPost, data.onergy_js_ctx.usrid);
+            await onergy.InsertManyOnergy(arrayPost, fdtdIdPost, data.onergy_js_ctx.usrid);
         }
 
         let postInfo = {
@@ -87,7 +179,7 @@ function init(json) {
                 fdtIdSaveRegistros: fdtdIdPost,
             },
         };
-        onergy_updatemany({
+        await onergy_updatemany({
             fdtid: '8b7dc946-2993-44e9-932a-947b77eb44cf',
             assid: data.onergy_js_ctx.assid,
             usrid: data.onergy_js_ctx.usrid,
@@ -98,6 +190,7 @@ function init(json) {
     }
 
     return true;
+    return SetObjectResponse(true, data, true);
 }
 
 function initBefore(json) {
@@ -109,7 +202,7 @@ function initDelete(json) {
 function SetObjectResponse(cond, json, WaitingWebHook) {
     if (WaitingWebHook === undefined) WaitingWebHook = false;
 
-    var obj = {
+    let obj = {
         cond: cond,
         json: JSON.stringify(json),
         WaitingWebHook: WaitingWebHook,
@@ -117,28 +210,6 @@ function SetObjectResponse(cond, json, WaitingWebHook) {
     return obj;
 }
 
-function getOnergyItem(fdtid, assid, usrid, filtro) {
-    let keepSearching = true;
-    let skip = 0;
-    take = 500;
-    let result = [];
-    while (keepSearching) {
-        let strPageResp = onergy_get({
-            fdtid: fdtid,
-            assid: assid,
-            usrid: usrid,
-            filter: filtro,
-            skip: skip,
-            take: take,
-        });
-        skip += take;
-        let pageResp = JSON.parse(strPageResp);
-        if (pageResp != null && pageResp.length > 0) {
-            keepSearching = pageResp.length == take;
-            result = result.concat(pageResp);
-        } else {
-            keepSearching = false;
-        }
-    }
-    return JSON.stringify(result);
-}
+//!METODOS PADRAO ===
+const json = {};
+init(JSON.stringify(json));
