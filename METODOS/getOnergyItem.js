39ee12e325b90775fc182f3d2e3e0971d4d8edf0
jeferson-franco <jefersonfranco@pm.me>
2022-10-27 15:44:04 -0300

    update getOnergyItem.js

--- METODOS/getOnergyItem.js
+++ METODOS/getOnergyItem.js
@@ -11,20 +11,24 @@ let strFiltro = JSON.stringify([{ FielName: 'campo_de_busca', Type: 'string', Fi
 
 let cadastroLOG = await getOnergyItem(fdtidDoCardDeBusca, data.onergy_js_ctx.assid, data.onergy_js_ctx.usrid, strFiltro);
 
-async function getOnergyItem(fdtid, assid, usrid, filtro) {
+async function getOnergyItem(fdtid, assid, usrid, filtro, fedid) {
     let keepSearching = true;
     let skip = 0;
-    take = 500;
+    let take = 500;
     let result = [];
     while (keepSearching) {
-        let strPageResp = await onergy_get({
+        let onergyGetObj = {
             fdtid: fdtid,
             assid: assid,
             usrid: usrid,
             filter: filtro,
             skip: skip,
             take: take,
-        });
+        };
+        if (fedid) {
+            onergyGetObj.fedid = fedid;
+        }
+        let strPageResp = await onergy_get(onergyGetObj);
         skip += take;
         let pageResp = JSON.parse(strPageResp);
         if (pageResp != null && pageResp.length > 0) {
