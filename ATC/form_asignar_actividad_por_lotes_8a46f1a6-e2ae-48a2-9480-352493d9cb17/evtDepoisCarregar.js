353e1289f036dc0cfd81e11e330552fa265c12aa
jefersonfranco <jefersonfranco@protonmail.com>
2023-01-27 22:03:37 -0300

    update evtDepoisCarregar.js

--- ATC/form_asignar_actividad_por_lotes_8a46f1a6-e2ae-48a2-9480-352493d9cb17/evtDepoisCarregar.js
+++ ATC/form_asignar_actividad_por_lotes_8a46f1a6-e2ae-48a2-9480-352493d9cb17/evtDepoisCarregar.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 let onergyCtx = mtdOnergy.JsEvtGetCurrentCtx();
 
 let mainMethod = async () => {
@@ -6,7 +7,9 @@ let mainMethod = async () => {
 };
 
 let aplicarDataLote = async () => {
+    // eslint-disable-next-line no-undef
     await mtdOnergy.JsEvtGetParentRefID();
+    // eslint-disable-next-line no-undef
     let dataLote = mtdOnergy.JsEvtGetItemValue('data_lote');
     if (dataLote == undefined || dataLote == '') {
         let date = new Date();
@@ -15,32 +18,40 @@ let aplicarDataLote = async () => {
             month: date.getMonth() + 1,
             day: date.getDate(),
         };
+        // eslint-disable-next-line no-undef
         mtdOnergy.JsEvtSetItemValue('data_lote', dt_obj);
     }
 };
 
 let aplicarNumeroLote = async () => {
+    // eslint-disable-next-line no-undef
     await mtdOnergy.JsEvtGetParentRefID();
+    // eslint-disable-next-line no-undef
     mtdOnergy.JsEvtSetItemValue('CDE__status_lote', 'Procesado');
 
     let idAsignarActividadporLotes = '8a46f1a6-e2ae-48a2-9480-352493d9cb17';
+    // eslint-disable-next-line no-undef
     let getNumeroLote = await mtdOnergy.JsEvtGetFeedData({
         fdtID: idAsignarActividadporLotes,
     });
     if (getNumeroLote.length == undefined || getNumeroLote.length == 0) {
+        // eslint-disable-next-line no-undef
         mtdOnergy.JsEvtSetItemValue('CDE__numero_lote', 'LOT0001');
     } else {
         for (let item in getNumeroLote) {
             if (onergyCtx.fedid != getNumeroLote[item].id) {
+                // eslint-disable-next-line no-undef
                 let numeroLote = mtdOnergy.JsEvtGetItemValue('CDE__numero_lote');
                 let objNumeroLote = await getNumeroLote.filter((item) => item.urlJsonContext.CDE__numero_lote == numeroLote);
                 if (numeroLote.length > 0) {
                     if (objNumeroLote.urlJsonContext.CDE__numero_lote == numeroLote) {
                         return;
                     } else {
+                        // eslint-disable-next-line no-undef
                         mtdOnergy.JsEvtSetItemValue('CDE__numero_lote', 'LOT000' + (getNumeroLote.length + 1));
                     }
                 } else {
+                    // eslint-disable-next-line no-undef
                     mtdOnergy.JsEvtSetItemValue('CDE__numero_lote', 'LOT000' + (getNumeroLote.length + 1));
                 }
             }
